531d0f36bad9af91c690eae188c8086afaf6bac5
uykarma <84086007+uykarma@users.noreply.github.com>
2021-05-12 13:07:45 -0300

    Update EmpJornalero.java

--- Tarea/src/Logica/EmpJornalero.java
+++ Tarea/src/Logica/EmpJornalero.java
@@ -3,4 +3,11 @@ package Logica;
 public class EmpJornalero extends Empleado {
     private double valorHora;
     private double horasTrabajadas;
+    
+    public double getValorHora() {
+    	return valorHora;
+    }
+    public double getHorasTrabajadas() {
+    	return horasTrabajadas;
+    }
 }
