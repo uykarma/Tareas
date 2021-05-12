c6cfde30709e2e33bee712d04571f45095af4d5c
Manuel <84086007+uykarma@users.noreply.github.com>
2021-05-12 15:12:13 -0300

    Update EmpMensual.java

--- Tarea/src/Logica/EmpMensual.java
+++ Tarea/src/Logica/EmpMensual.java
@@ -3,4 +3,11 @@ package Logica;
 public class EmpMensual extends Empleado {
     private double salarioMensual;
     private String tipoRegimen;
+
+    public double getSalarioMensual() {
+        return salarioMensual;
+    }
+    public String getTipoRegimen() {
+        return tipoRegimen;
+    }
 }
