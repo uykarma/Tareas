d82623ae71251a49e6de2b82d4b22fe561298207
Nawel-Gasparini <84103030+Nawel-Gasparini@users.noreply.github.com>
2021-05-12 17:47:05 -0300

    Update EmpMensual.java
    Setters, Contructores Getters

--- Tarea/src/Logica/EmpMensual.java
+++ Tarea/src/Logica/EmpMensual.java
@@ -3,11 +3,21 @@ package Logica;
 public class EmpMensual extends Empleado {
     private double salarioMensual;
     private String tipoRegimen;
+    
+    this.salarioMensual = 0.0;
+	this.tipoRegimen = "";
 
-    public double getSalarioMensual() {
-        return salarioMensual;
-    }
-    public String getTipoRegimen() {
-        return tipoRegimen;
-    }
+    public double getsalarioMensual() {
+		  return this.salarioMensual;
+	  }
+	  public void setsalarioMensual() {
+		   this.salarioMensual = salarioMensual;
+	  }   
+	  public double gettipoRegimen() {
+		  return this.tipoRegimen;
+	  }
+	  public void settipoRegimen() {
+		   this.tipoRegimen = tipoRegimen;
+	  }
+
 }
